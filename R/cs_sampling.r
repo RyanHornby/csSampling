4a3f71d28b1d0a5bcd17b7f2dee99dd0528d65f8
Ryan Hornby <ryanhornby1999@gmail.com>
2022-09-25 14:40:50 -0700

    Updated helper function to fix issue #4

--- R/cs_sampling.r
+++ R/cs_sampling.r
@@ -383,14 +383,27 @@ DEadj <- function(par, par_hat, R2R1){
 #' @param rindex - the row index to subset
 #' @return a named list of types as the input, with elements subset to rows index provided
 #' @export
-list_2D_row_subset <- function(nmlist, rindex){
+list_2D_row_subset <- function (nmlist, rindex) 
+{
   temp_list <- list()
-  for (k in 1:length(nmlist)){ 
-    ldim <- length(dim(nmlist[[k]]))
-    lcommas <- paste(rep(",", ldim -1), collapse = " ")
+  for (k in 1:length(nmlist)) {
+    tmpdim <- dim(nmlist[[k]])
+    ldim <- length(tmpdim)
+    lcommas <- paste(rep(",", ldim - 1), collapse = " ")
+    #copy over to new list - drop = FALSE retains ALL dimensions
     eval(parse(text = paste("temp_list$", names(nmlist)[k], 
-                            " <- ", "(nmlist$", names(nmlist)[k], ")[rindex", lcommas, "]", 
-                            sep = "")))
+                            " <- ", "(nmlist$", names(nmlist)[k], 
+                            ")[rindex", lcommas, ",drop = FALSE]", sep = "")))
+    #drop only first dimension of array - not others of size 1
+    if(ldim > 1){
+      eval(parse(text = paste("temp_list$", names(nmlist)[k], 
+                              " <- ", "array(temp_list$", names(nmlist)[k], ", dim = tmpdim[-1])", sep = "")))
+    }
+    #if only had 1 dim which is the MCMC draw, make a double (no dim), rather than an array of dim 1 or 0
+    if(ldim == 1){
+      eval(parse(text = paste("temp_list$", names(nmlist)[k], 
+                              " <- ", "as.double(temp_list$", names(nmlist)[k], ")", sep = "")))
+    }	  	
   }
   return(temp_list)
 }
